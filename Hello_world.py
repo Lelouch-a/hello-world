e9d6c2ce77a8ce01c6b2c8d264d6c9c22d80b087
Aamir Yusuf <yaamir7@gmail.com>
2021-09-30 18:59:45 +0100

    Add files via upload

---
+++ Hello_world.py
@@ -0,0 +1,14 @@
+Hello_world.py
+
+import pandas
+import tkinter
+
+
+print ("Hello_world")
+
+#HCFile = tkinter.filedialog.askopenfilename(parent=root, title="Select the Head Count File", defaultextension = ".xlsx")
+
+
+
+
+
